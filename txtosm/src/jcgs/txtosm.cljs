f0407ba30c6209965256c8ea8505137a3ccf49e5
John Sturdy <jcg.sturdy@gmail.com>
2022-01-26 11:25:51 +0000

    try some experiments
    This produces a page when run with:
      clj -M:fig:build
    but the REPL isn't active.
    
    If I uncomment the require of overpass-frontend, it breaks completely.

--- txtosm/src/jcgs/txtosm.cljs
+++ txtosm/src/jcgs/txtosm.cljs
@@ -3,20 +3,21 @@
    [goog.dom :as gdom]
    [reagent.dom :as rdom]
    [reagent.core :as r]
-   [overpass-frontend :as ovps]
+   ;; [overpass-frontend :as ovps] ; TODO: nothing works if this is not commented out
    ))
 
-(println "This text is printed from src/jcgs/txtosm.cljs. Go ahead and edit it and see reloading in action.")
+(println "This text is printed from src/jcgs/txtosm.cljs. Go ahead and edit it and see reloading in action A.")
 
-(js/console.log ovps)
+;; (js/console.log ovps)
+(js/console.log "ovps")
 
 (defn multiply [a b] (* a b))
 
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom
                     {:text "Hello textually mapped world!"
-                     :title "street name to go here"
-                     :api-results []
+                     :title "street name to go here followed by things"
+                     :api-results ["thing one" "thing two"]
                      :rows []}))
 
 (defn title []
@@ -26,26 +27,30 @@
   (gdom/getElement "app"))
 
 (defn debug-item [item]
-  [:div {:class "debugitem"}
-   (str item)])
+  [:li {:class "debugitem"}
+   (str item " for debug")])
 
 (defn app []
   [:div {:class "app"}
    [title]
    [:div {:class "textualmap"}
-    (for [item (vals (:api-results @app-state))]
+    [:ul
+    (for [item (:api-results @app-state)]
       [debug-item item])
-    ]])
+     ]
+    ]
+   ])
 
 (defn mount-app-element []
   (rdom/render [app] (get-app-element)))
 
 (mount-app-element)
 
-;; specify reload hook with ^:after-load metadata
+;; ;; specify reload hook with ^:after-load metadata
 (defn ^:after-load on-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  (mount-app-element)                   ; TODO: should this be here?
+  )
 
