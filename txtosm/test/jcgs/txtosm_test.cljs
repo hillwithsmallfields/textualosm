adf780407f19d221e1c28af99960e69a1f132bd4
John Sturdy <jcg.sturdy@gmail.com>
2022-01-27 11:49:40 +0000

    Reduce the test program so it doesn't make me keep 'multiply' in the main one

--- txtosm/test/jcgs/txtosm_test.cljs
+++ txtosm/test/jcgs/txtosm_test.cljs
@@ -1,10 +1,11 @@
 (ns jcgs.txtosm-test
     (:require
      [cljs.test :refer-macros [deftest is testing]]
-     [jcgs.txtosm :refer [multiply]]))
+     ;; [jcgs.txtosm :refer [multiply]]
+     ))
 
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
+;; (deftest multiply-test
+;;   (is (= (* 1 2) (multiply 1 2))))
 
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
+;; (deftest multiply-test-2
+;;   (is (= (* 75 10) (multiply 10 75))))
