12a18101a91f06d75491dc094d5c740dab38df27
John Sturdy <jcg.sturdy@gmail.com>
2026-02-18 10:13:18 +0000

    remove old javascript code

--- aside/txtosm/test/jcgs/test_runner.cljs
+++
@@ -1,9 +0,0 @@
-;; This test runner is intended to be run from the command line
-(ns jcgs.test-runner
-  (:require
-    ;; require all the namespaces that you want to test
-    [jcgs.txtosm-test]
-    [figwheel.main.testing :refer [run-tests-async]]))
-
-(defn -main [& args]
-  (run-tests-async 5000))
